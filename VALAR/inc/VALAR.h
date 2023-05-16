a29062a0fa7ecdd729bb41c351cd0e9dd46e6de9
Tenacious D <marissa.du.bois@intel.com>
2023-05-16 15:20:00 -0700

    VALAR-API Commit

---
+++ VALAR/inc/VALAR.h
@@ -0,0 +1,129 @@
+// Copyright (C) 2022 Intel Corporation
+
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"),
+// to deal in the Software without restriction, including without limitation
+// the rights to use, copy, modify, merge, publish, distribute, sublicense,
+// and/or sell copies of the Software, and to permit persons to whom
+// the Software is furnished to do so, subject to the following conditions:
+
+// The above copyright notice and this permission notice shall be included
+// in all copies or substantial portions of the Software.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+// OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES
+// OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+// ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE
+// OR OTHER DEALINGS IN THE SOFTWARE.
+#pragma once
+
+#define EMBED_VALAR_SHADERS
+
+namespace Intel
+{
+    typedef enum VALAR_AXIS_SHADING_RATE {
+        VALAR_AXIS_SHADING_RATE_1X = 0,
+        VALAR_AXIS_SHADING_RATE_2X = 0x1,
+        VALAR_AXIS_SHADING_RATE_4X = 0x2
+    } VALAR_AXIS_SHADING_RATE;
+
+    typedef enum VALAR_VARIABLE_SHADING_RATE_TIER {
+        VALAR_VARIABLE_SHADING_RATE_TIER_NOT_SUPPORTED = 0,
+        VALAR_VARIABLE_SHADING_RATE_TIER_1 = 1,
+        VALAR_VARIABLE_SHADING_RATE_TIER_2 = 2
+    } VALAR_VARIABLE_SHADING_RATE_TIER;
+
+    typedef enum VALAR_SHADING_RATE {
+        VALAR_SHADING_RATE_1X1 = 0,
+        VALAR_SHADING_RATE_1X2 = 0x1,
+        VALAR_SHADING_RATE_2X1 = 0x4,
+        VALAR_SHADING_RATE_2X2 = 0x5,
+        VALAR_SHADING_RATE_2X4 = 0x6,
+        VALAR_SHADING_RATE_4X2 = 0x9,
+        VALAR_SHADING_RATE_4X4 = 0xa
+    } VALAR_SHADING_RATE;
+
+    typedef enum VALAR_SHADING_RATE_COMBINER {
+        VALAR_SHADING_RATE_COMBINER_PASSTHROUGH = 0,
+        VALAR_SHADING_RATE_COMBINER_OVERRIDE = 1,
+        VALAR_SHADING_RATE_COMBINER_MIN = 2,
+        VALAR_SHADING_RATE_COMBINER_MAX = 3,
+        VALAR_SHADING_RATE_COMBINER_SUM = 4
+    } VALAR_SHADING_RATE_COMBINER;
+
+    typedef enum VALAR_RETURN_CODE {
+        VALAR_RETURN_CODE_SUCCESS,
+        VALAR_RETURN_CODE_ROOTSIG_FAIL,
+        VALAR_RETURN_CODE_PSO_FAIL,
+        VALAR_RETURN_CODE_INVALID_ARGUMENT,
+        VALAR_RETURN_CODE_INVALID_DEVICE,
+        VALAR_RETURN_CODE_NOT_SUPPORTED,
+        VALAR_RETURN_CODE_INITIALIZED,
+        VALAR_RETURN_CODE_NOT_INITIALIZED,
+        VALAR_RETURN_CODE_MAX
+    } VALAR_RETURN_CODE;
+
+    typedef enum VALAR_SHADER_PERMUTATIONS {
+        VALAR_SHADER_8X8,
+        VALAR_SHADER_16X16,
+        VALAR_DEBUG_SHADER_8X8,
+        VALAR_DEBUG_SHADER_16X16,
+        VALAR_SHADER_COUNT
+    } VALAR_SHADER_PERMUTATIONS;
+
+    struct VALAR_DESCRIPTOR_OPAQUE;
+
+    struct VALAR_HARDWARE_FEATURES
+    {
+        UINT                                m_shadingRateTileSize               = 0;
+        bool                                m_additionalShadingRatesSupported   = false;
+        bool                                m_sumCombinerSupported              = false;
+        bool                                m_meshShaderPerPrimSupported        = false;
+        VALAR_VARIABLE_SHADING_RATE_TIER    m_shadingRateTier                   = {};
+        bool                                m_vrsTier1Support                   = false;
+        bool                                m_vrsTier2Support                   = false;
+        bool                                m_isVALARSupported                  = false;
+    };
+
+    struct VALAR_DESCRIPTOR
+    {
+        VALAR_DESCRIPTOR();
+
+        VALAR_SHADING_RATE                  m_baseShadingRate                   = VALAR_SHADING_RATE_1X1;
+        float                               m_sensitivityThreshold              = 0.50f;
+        float                               m_quarterRateShadingModifier        = 2.13f;
+        float                               m_environmentLuminance              = 0.02f;
+        bool                                m_allowQuarterRateShading           = true;
+        bool                                m_weberFechnerMode                  = false;
+        float                               m_weberFechnerConstant              = 1.0f;
+        bool                                m_useMotionVectors                  = false;
+        bool                                m_useUpscaleMotionVectors           = false;
+        bool                                m_debugOverlay                      = false;
+        bool                                m_enabled                           = true;
+        UINT                                m_bufferWidth                       = 0;
+        UINT                                m_bufferHeight                      = 0;
+        UINT                                m_upscaleWidth                      = 0;
+        UINT                                m_upscaleHeight                     = 0;
+        ID3D12Device*                       m_device                            = nullptr;
+        ID3D12DescriptorHeap*               m_uavHeap                           = nullptr;
+        ID3D12Resource*                     m_valarBuffer                       = nullptr;
+        ID3D12Resource*                     m_colorBuffer                       = nullptr;
+        ID3D12Resource*                     m_velocityBuffer                    = nullptr;
+        ID3DBlob*                           m_shaderBlobs[VALAR_SHADER_COUNT];
+        ID3D12GraphicsCommandList5*         m_commandList                       = nullptr;
+        VALAR_DESCRIPTOR_OPAQUE*            m_pOpaque;
+        VALAR_HARDWARE_FEATURES             m_hwFeatures;
+    };
+
+    const VALAR_RETURN_CODE VALAR_Initialize(VALAR_DESCRIPTOR& desc);
+    const VALAR_RETURN_CODE VALAR_Release(const VALAR_DESCRIPTOR& desc);
+    const VALAR_RETURN_CODE VALAR_ComputeMask(const VALAR_DESCRIPTOR& desc);
+    const VALAR_RETURN_CODE VALAR_DebugOverlay(const VALAR_DESCRIPTOR& desc);
+    const VALAR_RETURN_CODE VALAR_ApplyMask(const VALAR_DESCRIPTOR& desc);
+    const VALAR_RETURN_CODE VALAR_ResetMask(const VALAR_DESCRIPTOR& desc);
+    const VALAR_RETURN_CODE VALAR_SetScreenSpaceCombiners(const VALAR_DESCRIPTOR& desc);
+    const VALAR_RETURN_CODE VALAR_SetHeroAssetCombiners(const VALAR_DESCRIPTOR& desc);
+    const VALAR_RETURN_CODE VALAR_SetCustomCombiners(const VALAR_DESCRIPTOR& desc, const VALAR_SHADING_RATE_COMBINER combiner1, const VALAR_SHADING_RATE_COMBINER combiner2);
+}
---
+++ VALAR/src/VALAROpaque.cpp
@@ -0,0 +1,481 @@
+// Copyright (C) 2023 Intel Corporation
+
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"),
+// to deal in the Software without restriction, including without limitation
+// the rights to use, copy, modify, merge, publish, distribute, sublicense,
+// and/or sell copies of the Software, and to permit persons to whom
+// the Software is furnished to do so, subject to the following conditions:
+
+// The above copyright notice and this permission notice shall be included
+// in all copies or substantial portions of the Software.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+// OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES
+// OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+// ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE
+// OR OTHER DEALINGS IN THE SOFTWARE.
+
+#include <wrl.h>
+#include <d3d12.h>
+#include <cassert>
+
+#include "VALAR.h"
+#include "VALAROpaque.h"
+
+#include "..\..\ThirdParty\d3dx12.h"
+
+#ifdef EMBED_VALAR_SHADERS
+    #include "Valar8x8CS.h"
+    #include "Valar16x16CS.h"
+    #include "ValarDebug8x8CS.h"
+    #include "ValarDebug16x16CS.h"
+#endif
+
+Intel::VALAR_DESCRIPTOR::VALAR_DESCRIPTOR()
+{
+    static VALAR_DESCRIPTOR_OPAQUE opaque;
+
+    m_pOpaque = &opaque;
+}
+
+const Intel::VALAR_RETURN_CODE Intel::VALAR_Initialize(Intel::VALAR_DESCRIPTOR & desc)
+{
+    if (!desc.m_pOpaque->m_isInitialized) {
+        if (desc.m_device == nullptr) {
+            return VALAR_RETURN_CODE_INVALID_DEVICE;
+        }
+        
+        VALAR_RETURN_CODE retCode = CheckHardwareSupport(desc, desc.m_hwFeatures);
+        if (retCode != VALAR_RETURN_CODE_SUCCESS) {
+            return VALAR_RETURN_CODE_NOT_SUPPORTED;
+        }
+        
+        retCode = CreateVALARRootSignature(desc);
+        if (retCode != VALAR_RETURN_CODE_SUCCESS) {
+            return retCode;
+        }
+
+        if (desc.m_hwFeatures.m_shadingRateTileSize == INTEL_TILE_SIZE) {
+            retCode = LoadShader(desc, VALAR_SHADER_8X8);
+            if (retCode != VALAR_RETURN_CODE_SUCCESS) {
+                return retCode;
+            }
+
+            retCode = LoadShader(desc, VALAR_DEBUG_SHADER_8X8);
+            if (retCode != VALAR_RETURN_CODE_SUCCESS) {
+                return retCode;
+            }
+        } else {
+            retCode = LoadShader(desc, VALAR_SHADER_16X16);
+            if (retCode != VALAR_RETURN_CODE_SUCCESS) {
+                return retCode;
+            }
+
+            retCode = LoadShader(desc, VALAR_DEBUG_SHADER_16X16);
+            if (retCode != VALAR_RETURN_CODE_SUCCESS) {
+                return retCode;
+            }
+        }
+
+        desc.m_pOpaque->m_device = desc.m_device;
+        desc.m_pOpaque->m_isInitialized = true;
+        desc.m_pOpaque->m_featureSupport = desc.m_hwFeatures;
+    } else {
+        return VALAR_RETURN_CODE_INITIALIZED;
+    }
+
+    return Intel::VALAR_RETURN_CODE_SUCCESS;
+}
+
+Intel::VALAR_RETURN_CODE Intel::CheckHardwareSupport(Intel::VALAR_DESCRIPTOR& desc, Intel::VALAR_HARDWARE_FEATURES& featureSupport)
+{
+     Intel::VALAR_RETURN_CODE retCode = VALAR_RETURN_CODE_NOT_SUPPORTED;
+
+     // Check for VRS hardware support
+     D3D12_FEATURE_DATA_D3D12_OPTIONS6 options = {};
+     D3D12_FEATURE_DATA_D3D12_OPTIONS10 options2 = {};
+
+     featureSupport.m_shadingRateTier = (VALAR_VARIABLE_SHADING_RATE_TIER)D3D12_VARIABLE_SHADING_RATE_TIER_NOT_SUPPORTED;
+     featureSupport.m_additionalShadingRatesSupported = 0;
+     featureSupport.m_shadingRateTileSize = 0;
+
+     if (SUCCEEDED(desc.m_device->CheckFeatureSupport(D3D12_FEATURE_D3D12_OPTIONS6, &options, sizeof(options)))) {
+         featureSupport.m_shadingRateTier = (VALAR_VARIABLE_SHADING_RATE_TIER)options.VariableShadingRateTier;
+
+         if (featureSupport.m_shadingRateTier >= D3D12_VARIABLE_SHADING_RATE_TIER_1) {
+             featureSupport.m_additionalShadingRatesSupported = options.AdditionalShadingRatesSupported;
+             featureSupport.m_vrsTier1Support = true;
+         }
+
+         if (featureSupport.m_shadingRateTier >= D3D12_VARIABLE_SHADING_RATE_TIER_2) {
+             featureSupport.m_shadingRateTileSize = options.ShadingRateImageTileSize;
+             featureSupport.m_vrsTier2Support = true;
+             featureSupport.m_isVALARSupported = true;
+         }
+
+         retCode = VALAR_RETURN_CODE_SUCCESS;
+     }
+
+     if (SUCCEEDED(desc.m_device->CheckFeatureSupport(D3D12_FEATURE_D3D12_OPTIONS10, &options2, sizeof(options2)))) {
+         featureSupport.m_sumCombinerSupported = options2.VariableRateShadingSumCombinerSupported;
+         featureSupport.m_meshShaderPerPrimSupported = options2.MeshShaderPerPrimitiveShadingRateSupported;
+     }
+
+     return retCode;
+ }
+
+ Intel::VALAR_RETURN_CODE Intel::CreateVALARRootSignature(Intel::VALAR_DESCRIPTOR& desc)
+ {
+     ComPtr<ID3DBlob> signature, errors;
+
+     CD3DX12_DESCRIPTOR_RANGE1 descRange[2] = {};
+     CD3DX12_ROOT_PARAMETER1 rootParams[2] = {};
+     D3D12_STATIC_SAMPLER_DESC sampler = {};
+     D3D12_FEATURE_DATA_ROOT_SIGNATURE featureData = {};
+     CD3DX12_VERSIONED_ROOT_SIGNATURE_DESC rootSignatureDesc;
+
+     featureData.HighestVersion = D3D_ROOT_SIGNATURE_VERSION_1_1;
+
+     if (FAILED(desc.m_device->CheckFeatureSupport(D3D12_FEATURE_ROOT_SIGNATURE, &featureData, sizeof(featureData)))) {
+         featureData.HighestVersion = D3D_ROOT_SIGNATURE_VERSION_1_0;
+     }
+
+     descRange[0].Init(D3D12_DESCRIPTOR_RANGE_TYPE_UAV, 4, 0);
+     descRange[1].Init(D3D12_DESCRIPTOR_RANGE_TYPE_CBV, 13, 0, D3D12_DESCRIPTOR_RANGE_FLAG_DATA_STATIC);
+
+     rootParams[0].InitAsConstants(12, 0);
+     rootParams[1].InitAsDescriptorTable(1, &descRange[0]);
+     rootSignatureDesc.Init_1_1(_countof(rootParams), rootParams, 0, nullptr, D3D12_ROOT_SIGNATURE_FLAG_NONE);
+
+     if (FAILED(D3D12SerializeVersionedRootSignature(&rootSignatureDesc, &signature, &errors))) {
+         return VALAR_RETURN_CODE_ROOTSIG_FAIL;
+     }
+
+     auto sig = desc.m_pOpaque->m_valarRootSignature.Get();
+
+     if (FAILED(desc.m_device->CreateRootSignature(0, signature->GetBufferPointer(), signature->GetBufferSize(), IID_PPV_ARGS(&sig)))) {
+         return VALAR_RETURN_CODE_ROOTSIG_FAIL;
+     }
+     desc.m_pOpaque->m_valarRootSignature = sig;
+
+     return VALAR_RETURN_CODE_SUCCESS;
+ }
+
+Intel::VALAR_RETURN_CODE Intel::LoadShader(Intel::VALAR_DESCRIPTOR& desc, VALAR_SHADER_PERMUTATIONS permutation)
+{
+    UINT8* pComputeShaderData = nullptr;
+    UINT computeShaderDataLength = 0;
+
+#ifdef EMBED_VALAR_SHADERS
+    switch (permutation)
+    {
+    case VALAR_SHADER_8X8:
+        pComputeShaderData = (UINT8*)g_valar8x8ByteCode;
+        computeShaderDataLength = sizeof(g_valar8x8ByteCode) / sizeof(const unsigned char);
+        break;
+    case VALAR_SHADER_16X16:
+        pComputeShaderData = (UINT8*)g_valar16x16ByteCode;
+        computeShaderDataLength = sizeof(g_valar16x16ByteCode) / sizeof(const unsigned char);
+        break;
+    case VALAR_DEBUG_SHADER_8X8:
+        pComputeShaderData = (UINT8*)g_valar8x8DebugByteCode;
+        computeShaderDataLength = sizeof(g_valar8x8DebugByteCode) / sizeof(const unsigned char);
+        break;
+    case VALAR_DEBUG_SHADER_16X16:
+        pComputeShaderData = (UINT8*)g_valar16x16DebugByteCode;
+        computeShaderDataLength = sizeof(g_valar16x16DebugByteCode) / sizeof(const unsigned char);
+        break;
+    }
+#else
+    if (desc.m_shaderBlobs[permutation] == nullptr)
+        return VALAR_RETURN_CODE_INVALID_ARGUMENT;
+
+    pComputeShaderData = (UINT8*)desc.m_shaderBlobs[permutation]->GetBufferPointer();
+    computeShaderDataLength = (UINT)desc.m_shaderBlobs[permutation]->GetBufferSize();
+#endif
+
+    D3D12_COMPUTE_PIPELINE_STATE_DESC psoDesc = {};
+    psoDesc.pRootSignature = desc.m_pOpaque->m_valarRootSignature.Get();
+    psoDesc.CS = CD3DX12_SHADER_BYTECODE(pComputeShaderData, computeShaderDataLength);
+   
+    if (FAILED(desc.m_device->CreateComputePipelineState(&psoDesc, IID_PPV_ARGS(&desc.m_pOpaque->m_valarShaderPermutations[permutation])))) {
+        return VALAR_RETURN_CODE_PSO_FAIL;
+    }
+
+    return VALAR_RETURN_CODE_SUCCESS;
+}
+
+
+const Intel::VALAR_RETURN_CODE Intel::VALAR_Release(const Intel::VALAR_DESCRIPTOR& desc)
+{
+    VALAR_RETURN_CODE retCode = VALAR_RETURN_CODE_NOT_INITIALIZED;
+
+    if (desc.m_pOpaque->m_isInitialized)
+    {
+        VALAR_SAFE_RELEASE(desc.m_pOpaque->m_valarShaderPermutations[VALAR_SHADER_8X8]);
+        VALAR_SAFE_RELEASE(desc.m_pOpaque->m_valarShaderPermutations[VALAR_SHADER_16X16]);
+        VALAR_SAFE_RELEASE(desc.m_pOpaque->m_valarShaderPermutations[VALAR_DEBUG_SHADER_8X8]);
+        VALAR_SAFE_RELEASE(desc.m_pOpaque->m_valarShaderPermutations[VALAR_DEBUG_SHADER_16X16]);
+        VALAR_SAFE_RELEASE(desc.m_pOpaque->m_valarRootSignature);
+        VALAR_SAFE_RELEASE(desc.m_pOpaque->m_device);
+
+        desc.m_pOpaque->m_isInitialized = false;
+        retCode = VALAR_RETURN_CODE_SUCCESS;
+    }
+
+    return retCode;
+}
+
+const Intel::VALAR_RETURN_CODE Intel::VALAR_DebugOverlay(const VALAR_DESCRIPTOR& desc)
+{
+    if (!desc.m_hwFeatures.m_vrsTier2Support) {
+        return VALAR_RETURN_CODE_NOT_SUPPORTED;
+    }
+
+    if (desc.m_commandList == nullptr) {
+        return VALAR_RETURN_CODE_INVALID_ARGUMENT;
+    }
+
+    if (desc.m_colorBuffer == nullptr) {
+        return VALAR_RETURN_CODE_INVALID_ARGUMENT;
+    }
+
+    if (desc.m_valarBuffer == nullptr) {
+        return VALAR_RETURN_CODE_INVALID_ARGUMENT;
+    }
+
+    if (desc.m_pOpaque->m_device == nullptr) {
+        return VALAR_RETURN_CODE_INVALID_DEVICE;
+    }
+
+    if (!desc.m_pOpaque->m_isInitialized) {
+        return VALAR_RETURN_CODE_NOT_INITIALIZED;
+    }
+
+    if (desc.m_enabled && desc.m_debugOverlay)
+    {
+        auto barrier = CD3DX12_RESOURCE_BARRIER::Transition(desc.m_valarBuffer,
+            D3D12_RESOURCE_STATE_SHADING_RATE_SOURCE,
+            D3D12_RESOURCE_STATE_UNORDERED_ACCESS);
+        desc.m_commandList->ResourceBarrier(1, &barrier);
+
+        ID3D12DescriptorHeap* ppHeapsCompute[] = { desc.m_uavHeap };
+        desc.m_commandList->SetDescriptorHeaps(_countof(ppHeapsCompute), ppHeapsCompute);
+        desc.m_commandList->SetComputeRootSignature(desc.m_pOpaque->m_valarRootSignature.Get());
+        desc.m_commandList->SetComputeRootDescriptorTable(1, desc.m_uavHeap->GetGPUDescriptorHandleForHeapStart());
+
+        if (desc.m_pOpaque->m_featureSupport.m_shadingRateTileSize == INTEL_TILE_SIZE) {
+            desc.m_commandList->SetPipelineState(desc.m_pOpaque->m_valarShaderPermutations[VALAR_DEBUG_SHADER_8X8].Get());
+        } else if (desc.m_pOpaque->m_featureSupport.m_shadingRateTileSize == OTHER_TILE_SIZE) {
+            desc.m_commandList->SetPipelineState(desc.m_pOpaque->m_valarShaderPermutations[VALAR_DEBUG_SHADER_16X16].Get());
+        } else {
+            assert(false);
+        }
+
+        desc.m_commandList->Dispatch(
+            (UINT)ceil((float)desc.m_bufferWidth / (float)desc.m_pOpaque->m_featureSupport.m_shadingRateTileSize),
+            (UINT)ceil((float)desc.m_bufferHeight / (float)desc.m_pOpaque->m_featureSupport.m_shadingRateTileSize), 1);
+
+        barrier = CD3DX12_RESOURCE_BARRIER::Transition(desc.m_valarBuffer,
+            D3D12_RESOURCE_STATE_UNORDERED_ACCESS,
+            D3D12_RESOURCE_STATE_SHADING_RATE_SOURCE);
+        desc.m_commandList->ResourceBarrier(1, &barrier);
+    }
+
+    return VALAR_RETURN_CODE_SUCCESS;
+}
+
+const Intel::VALAR_RETURN_CODE Intel::VALAR_ComputeMask(const Intel::VALAR_DESCRIPTOR& desc)
+{
+    if (!desc.m_hwFeatures.m_vrsTier2Support) {
+        return VALAR_RETURN_CODE_NOT_SUPPORTED;
+    }
+
+    if (desc.m_commandList == nullptr) {
+        return VALAR_RETURN_CODE_INVALID_ARGUMENT;
+    }
+        
+    if (desc.m_colorBuffer == nullptr) {
+        return VALAR_RETURN_CODE_INVALID_ARGUMENT;
+    }
+        
+    if (desc.m_valarBuffer == nullptr) {
+        return VALAR_RETURN_CODE_INVALID_ARGUMENT;
+    }
+    
+    if (desc.m_uavHeap == nullptr) {
+        return VALAR_RETURN_CODE_INVALID_ARGUMENT;
+    }
+
+    if (desc.m_pOpaque->m_device == nullptr) {
+        return VALAR_RETURN_CODE_INVALID_DEVICE;
+    }
+
+    if (!desc.m_pOpaque->m_isInitialized) {
+        return VALAR_RETURN_CODE_NOT_INITIALIZED;
+    }
+
+    if (desc.m_enabled) {
+        auto barrier = CD3DX12_RESOURCE_BARRIER::Transition(desc.m_valarBuffer,
+            D3D12_RESOURCE_STATE_SHADING_RATE_SOURCE,
+            D3D12_RESOURCE_STATE_UNORDERED_ACCESS);
+        desc.m_commandList->ResourceBarrier(1, &barrier);
+
+        ID3D12DescriptorHeap* ppHeapsCompute[] = { desc.m_uavHeap };
+        desc.m_commandList->SetDescriptorHeaps(_countof(ppHeapsCompute), ppHeapsCompute);
+        desc.m_commandList->SetComputeRootSignature(desc.m_pOpaque->m_valarRootSignature.Get());
+
+        VALAR_ROOT_CONSTANTS constants =
+        {
+            desc.m_bufferWidth,
+            desc.m_bufferHeight,
+            (UINT)desc.m_pOpaque->m_featureSupport.m_shadingRateTileSize,
+            desc.m_sensitivityThreshold,
+            desc.m_environmentLuminance,
+            desc.m_quarterRateShadingModifier,
+            desc.m_weberFechnerConstant,
+            desc.m_weberFechnerMode,
+            desc.m_useMotionVectors,
+            desc.m_allowQuarterRateShading,
+            desc.m_upscaleWidth,
+            desc.m_upscaleHeight,
+            desc.m_useUpscaleMotionVectors
+        };
+
+        desc.m_commandList->SetComputeRoot32BitConstants(0, 10, &constants, 0);
+        desc.m_commandList->SetComputeRootDescriptorTable(1, desc.m_uavHeap->GetGPUDescriptorHandleForHeapStart());
+
+        if (desc.m_pOpaque->m_featureSupport.m_shadingRateTileSize == INTEL_TILE_SIZE) {
+            desc.m_commandList->SetPipelineState(desc.m_pOpaque->m_valarShaderPermutations[VALAR_SHADER_8X8].Get());
+        } else if (desc.m_pOpaque->m_featureSupport.m_shadingRateTileSize == OTHER_TILE_SIZE) {
+            desc.m_commandList->SetPipelineState(desc.m_pOpaque->m_valarShaderPermutations[VALAR_SHADER_16X16].Get());
+        } else {
+            assert(false);
+        }
+
+        desc.m_commandList->Dispatch(
+            (UINT)ceil((float)desc.m_bufferWidth / (float)desc.m_pOpaque->m_featureSupport.m_shadingRateTileSize),
+            (UINT)ceil((float)desc.m_bufferHeight / (float)desc.m_pOpaque->m_featureSupport.m_shadingRateTileSize), 1);
+
+        barrier = CD3DX12_RESOURCE_BARRIER::Transition(desc.m_valarBuffer,
+            D3D12_RESOURCE_STATE_UNORDERED_ACCESS,
+            D3D12_RESOURCE_STATE_SHADING_RATE_SOURCE);
+        desc.m_commandList->ResourceBarrier(1, &barrier);
+    }
+
+    return VALAR_RETURN_CODE_SUCCESS;
+}
+
+const Intel::VALAR_RETURN_CODE Intel::VALAR_ApplyMask(const Intel::VALAR_DESCRIPTOR& desc)
+{
+    if (!desc.m_enabled) {
+        return VALAR_RETURN_CODE_SUCCESS;
+    }
+
+    if (!desc.m_hwFeatures.m_vrsTier2Support) {
+        return VALAR_RETURN_CODE_NOT_SUPPORTED;
+    }
+
+    if (desc.m_commandList == nullptr) {
+        return VALAR_RETURN_CODE_INVALID_ARGUMENT;
+    }    
+    
+    if (desc.m_valarBuffer == nullptr) {
+        return VALAR_RETURN_CODE_INVALID_ARGUMENT;
+    }
+
+    if (!desc.m_pOpaque->m_isInitialized) {
+        return VALAR_RETURN_CODE_NOT_INITIALIZED;
+    }
+
+    desc.m_commandList->RSSetShadingRateImage(desc.m_valarBuffer);
+
+    return VALAR_RETURN_CODE_SUCCESS;
+}
+
+const Intel::VALAR_RETURN_CODE Intel::VALAR_ResetMask(const Intel::VALAR_DESCRIPTOR& desc)
+{
+    if (!desc.m_hwFeatures.m_vrsTier2Support) {
+        return VALAR_RETURN_CODE_NOT_SUPPORTED;
+    }
+
+    if (desc.m_commandList == nullptr) {
+        return VALAR_RETURN_CODE_INVALID_ARGUMENT;
+    }
+
+    if (!desc.m_pOpaque->m_isInitialized) {
+        return VALAR_RETURN_CODE_NOT_INITIALIZED;
+    }
+
+    desc.m_commandList->RSSetShadingRateImage(nullptr);
+
+    return VALAR_RETURN_CODE_SUCCESS;
+}
+
+const Intel::VALAR_RETURN_CODE Intel::VALAR_SetScreenSpaceCombiners(const Intel::VALAR_DESCRIPTOR& desc)
+{
+    if (!desc.m_hwFeatures.m_vrsTier1Support) {
+        return VALAR_RETURN_CODE_NOT_SUPPORTED;
+    }
+
+    if (desc.m_commandList == nullptr) {
+        return VALAR_RETURN_CODE_INVALID_ARGUMENT;
+    }
+
+    if (!desc.m_pOpaque->m_isInitialized) {
+        return VALAR_RETURN_CODE_NOT_INITIALIZED;
+    }
+
+    D3D12_SHADING_RATE_COMBINER combiners[2] = { D3D12_SHADING_RATE_COMBINER_PASSTHROUGH, D3D12_SHADING_RATE_COMBINER_OVERRIDE };
+
+    desc.m_commandList->RSSetShadingRate((D3D12_SHADING_RATE)desc.m_baseShadingRate, combiners);
+    
+    return VALAR_RETURN_CODE_SUCCESS;
+}
+
+const Intel::VALAR_RETURN_CODE Intel::VALAR_SetHeroAssetCombiners(const Intel::VALAR_DESCRIPTOR& desc)
+{
+    if (!desc.m_hwFeatures.m_vrsTier1Support) {
+        return VALAR_RETURN_CODE_NOT_SUPPORTED;
+    }
+
+    if (desc.m_commandList == nullptr) {
+        return VALAR_RETURN_CODE_INVALID_ARGUMENT;
+    }
+
+    if (!desc.m_pOpaque->m_isInitialized) {
+        return VALAR_RETURN_CODE_NOT_INITIALIZED;
+    }
+
+    D3D12_SHADING_RATE_COMBINER combiners[2] = { D3D12_SHADING_RATE_COMBINER_PASSTHROUGH, D3D12_SHADING_RATE_COMBINER_PASSTHROUGH };
+
+    desc.m_commandList->RSSetShadingRate((D3D12_SHADING_RATE)desc.m_baseShadingRate, combiners);
+
+    return VALAR_RETURN_CODE_SUCCESS;
+}
+
+const Intel::VALAR_RETURN_CODE Intel::VALAR_SetCustomCombiners(const Intel::VALAR_DESCRIPTOR& desc, const VALAR_SHADING_RATE_COMBINER combiner1, const VALAR_SHADING_RATE_COMBINER combiner2)
+{
+    if (!desc.m_hwFeatures.m_vrsTier1Support) {
+        return VALAR_RETURN_CODE_NOT_SUPPORTED;
+    }
+
+    if (desc.m_commandList == nullptr) {
+        return VALAR_RETURN_CODE_INVALID_ARGUMENT;
+    }
+
+    if (!desc.m_pOpaque->m_isInitialized) {
+        return VALAR_RETURN_CODE_NOT_INITIALIZED;
+    }
+
+    D3D12_SHADING_RATE_COMBINER combiners[2] = { 
+        (D3D12_SHADING_RATE_COMBINER)combiner1, 
+        (D3D12_SHADING_RATE_COMBINER)combiner2 };
+
+    desc.m_commandList->RSSetShadingRate((D3D12_SHADING_RATE)desc.m_baseShadingRate, combiners);
+
+    return VALAR_RETURN_CODE_SUCCESS;
+}
